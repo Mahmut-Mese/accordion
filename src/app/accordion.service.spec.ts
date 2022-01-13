74a29acfc198ea62d73c73999d5d63d6b808d28a
Mahmut-Mese <39201076+Mahmut-Mese@users.noreply.github.com>
2022-01-13 03:30:50 +0000

    fix test bugs

--- src/app/accordion.service.spec.ts
+++ src/app/accordion.service.spec.ts
@@ -1,12 +1,17 @@
-import { TestBed } from '@angular/core/testing';
 
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { AccordionService } from './accordion.service';
 
 describe('AccordionService', () => {
   let service: AccordionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [AccordionService]
+    });
     service = TestBed.inject(AccordionService);
   });
 
@@ -14,3 +19,5 @@ describe('AccordionService', () => {
     expect(service).toBeTruthy();
   });
 });
+
+ 
